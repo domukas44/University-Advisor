253d0b3feae9f37cc348bf18024549e5960a8780
Gabriele <gabriele.juodyte@gmail.com>
2019-10-12 14:03:00 +0300

    Added Review class and basic serialization

--- Controllers/Subject.cs
+++ Controllers/Subject.cs
@@ -1,21 +1,33 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using University_advisor.Controllers;
 
 namespace University_advisor
 {
     public class Subject
     {
-        private string name;
-        private double rating;
+        public int id { get; }
+        private static int idNr = 0;
+        public string Name { get; set; }
+           
+        [JsonIgnore]     // kad prie kiekvieno review neįrašytų bendro dalyko reitingo
+        public double Rating { get; set; }
+
+        // private laukus serializer ignoruoja
+        private List<Review> reviewList { get; }
 
         public Subject(string name, double rating)
         {
-            this.name = name;
-            this.rating = rating;
+            id = idNr++;
+            Name = name;
+            Rating = rating;
+            reviewList = new List<Review>();
         }
 
-        public string Name { get { return name; } set { name = value; } }
-
-        public double Rating { get { return rating; } set { rating = value; } }
-
+        public void addReview(Review review)
+        {
+            reviewList.Add(review);
+        }
     }
 }
