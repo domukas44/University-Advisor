f3fc19848fa926ff88164a1c52c72c6b18769ac7
Zilvinas Bartkus <zbartkus@1clickfactory.com>
2019-10-20 21:03:31 +0300

    New class implements IAccount interface

---
+++ Controllers/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University_advisor.Controllers
+{
+    class User : IAccount
+    {
+        public string name { get; set; }
+
+        public string email { get; set; }
+
+        public string password { get; set; }
+
+        public bool checkIfExists(string name, string email, string password)
+        {
+            if ((this.name == name) && (this.email == email) && (this.password == password)) return true;
+            else return false;
+        }
+    }
+}
