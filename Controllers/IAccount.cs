d33a34acfef17f91c71a004e9c6989db0d00377d
Zilvinas Bartkus <zbartkus@1clickfactory.com>
2019-10-20 21:03:08 +0300

    Created new interface IAccount

---
+++ Controllers/IAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University_advisor.Controllers
+{
+    interface IAccount
+    {
+        string name { get; set; }
+        string email { get; set; }
+        string password { get; set; }
+        bool checkIfExists(string name, string email, string password);
+    }
+}
