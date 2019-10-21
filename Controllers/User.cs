7aa570173389bb454a7ef233fb79816fe022778b
Zilvinas Bartkus <zbartkus@1clickfactory.com>
2019-10-21 09:55:43 +0300

    Created methods in User class to create new User objects or throw error if same email exists. Reused serialization and deserialization.

--- Controllers/User.cs
+++ Controllers/User.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using University_advisor.Controllers;
 
 namespace University_advisor.Controllers
 {
-    class User : IAccount
+    public class User : IAccount
     {
         public string name { get; set; }
 
@@ -14,10 +17,74 @@ namespace University_advisor.Controllers
 
         public string password { get; set; }
 
-        public bool checkIfExists(string name, string email, string password)
+        public User(string name, string email, string password)
         {
-            if ((this.name == name) && (this.email == email) && (this.password == password)) return true;
-            else return false;
+            this.name = name;
+            this.email = email;
+            this.password = password;
+
+            if (!checkIfExists())
+                saveAccountToFile();
+            else
+                throw new ArgumentException("This email is already registered.");
         }
+
+        public User(string email, string password)
+        {
+            this.email = email;
+            this.password = password;
+        }
+
+        public User()
+        {
+
+        }
+
+        public bool checkIfExists()
+        {
+            List<User> users = getUserList();
+            if ((users.Find(x => x.email == this.email)) != null)
+                return true;
+            else
+                return false;
+        }
+
+        public bool checkIfExists(IAccount user)
+        {
+            List<User> users = getUserList();
+            if ((users.Find(x => x.email == user.email)) != null)
+                return true;
+            else
+                return false;
+        }
+
+        public List<User> getUserList()
+        {
+            var users = new List<User>();
+            using (StreamReader sr = new StreamReader(@"..\..\Resources\User.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    users.Add(JsonConvert.DeserializeObject<User>(sr.ReadLine()));
+                }
+            }
+
+            return users;
+        }
+
+        public void saveAccountToFile() 
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+            using (StreamWriter sw = new StreamWriter(@"..\..\Resources\User.txt", true))       // true means append to file
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, this);
+                sw.WriteLine();
+            }
+        }
+
+
+
     }
 }
