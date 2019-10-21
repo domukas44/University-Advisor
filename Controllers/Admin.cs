eb6dbdd7a12002123791b0f06bd7300cc43daa25
Zilvinas Bartkus <zbartkus@1clickfactory.com>
2019-10-21 09:54:05 +0300

    Add empty class Admin

---
+++ Controllers/Admin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University_advisor.Controllers
+{
+    class Admin
+    {
+    }
+}
