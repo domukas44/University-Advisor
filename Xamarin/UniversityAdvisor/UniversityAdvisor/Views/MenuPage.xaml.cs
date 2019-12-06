196c8d546dfca74e6954ec22b2fb9bfbc11ba314
Gabriele <gabriele.juodyte@gmail.com>
2019-12-06 13:42:33 +0200

    Now shows subject list in menu

--- Xamarin/UniversityAdvisor/UniversityAdvisor/Views/MenuPage.xaml.cs
+++ Xamarin/UniversityAdvisor/UniversityAdvisor/Views/MenuPage.xaml.cs
@@ -25,48 +25,19 @@ namespace UniversityAdvisor.Views
         public MenuPage()
         {
             InitializeComponent();
-
-            /*menuItems = new List<HomeMenuItem>
-            {
-                new HomeMenuItem {Id = 0, Title="Browse" },
-                new HomeMenuItem {Id = 1, Title="About" },
-                new HomeMenuItem {Id = 2, Title="Register" }
-
-            };
-
-            ListViewMenu.ItemsSource = menuItems;
-
-            ListViewMenu.SelectedItem = menuItems[0];
-            ListViewMenu.ItemSelected += async (sender, e) =>
-            {
-
-                //var user = (User)BindingContext;
-                if (e.SelectedItem == null)
-                    return;
-
-                var id = (int)((HomeMenuItem)e.SelectedItem).Id;
-                await RootPage.NavigateFromMenu(id);
-            };*/
-
-
-
-            ListViewMenu = new ListView();
             PopulateSubjectList();
-
-            /*menuItems = new List<HomeMenuItem>();
-            foreach (Subject s in ListViewMenu.ItemsSource)
-            {
-                menuItems.Add(new HomeMenuItem { Id = Convert.ToInt32(s.Id), Title = s.Name + "\t" + s.Rating });
-            };
-            ListViewMenu.ItemsSource = menuItems;*/
-
         }
 
         private async void PopulateSubjectList()
         {
             sds = new SubjectDataStore();
-            ListViewMenu.ItemsSource = await sds.GetItemsAsync();
-            await DisplayAlert("", "ok", "ok");
+            var subjects = await sds.GetItemsAsync(); 
+            menuItems = new List<HomeMenuItem>();
+            foreach (Subject s in subjects)
+            {
+                menuItems.Add(new HomeMenuItem { Id = Convert.ToInt32(s.Id), Title = s.Name + ", " + s.Rating });
+            };
+            ListViewMenu.ItemsSource = menuItems;
         }
     }
 }
